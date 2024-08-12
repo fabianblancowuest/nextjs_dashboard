812f2aa6188510050c03421285d137810e0dde7c
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-08-12 06:53:42 -0300

    Rutas, rutas anidadas y layouts

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Invoices = () => {
+	return <div className="text-center">Dashboad Invoices</div>;
+};
+
+export default Invoices;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { montserrat } from "./ui/fonts";
 import "./ui/global.css";
+import "../app/ui/styles/footer.css";
 
 export default function RootLayout({
 	children,
@@ -8,7 +9,14 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={`${montserrat.className} antialiased`}>{children}</body>
+			<body className={`${montserrat.className} antialiased`}>
+				<div className="__container__">
+					{children}
+					<footer className="py-10 flex justify-center items-center footer">
+						<p>Created with ❤ by Fabian Blanco Wuest (2024) ©</p>
+					</footer>
+				</div>
+			</body>
 		</html>
 	);
 }
